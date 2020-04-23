e3f56aceeeeb62e3daf5110f095fd19a93aa87c8
abuturabiftikhar <63926829+abuturabiftikhar@users.noreply.github.com>
2020-04-23 23:25:29 +0500

    Add files via upload

---
+++ First (Chapter 2).py
@@ -0,0 +1,19 @@
+# Variables
+# Type:Integer
+number = 8
+number2 = 4
+print (number + number2)   
+print (number - number2)
+print (number * number2)
+print (number / number2)
+# Type:Floating Point
+number3 = 8.4
+number4 = 5.9
+print (number3 + number4)
+print (number3 - number4)
+print (number3 * number4)
+print (number3 / number4)
+# Type:String
+# Stairs Pattern
+message = "king" '\n' '\t' "king" '\n' '\t' '\t' "king" '\n' '\t' '\t' '\t' "king" '\n' '\t' '\t' '\t' '\t' "king" '\n' '\t' '\t' '\t' '\t' '\t' "king" '\n' '\t' '\t' '\t' '\t' '\t' '\t' "king"
+print (message)
---
+++ Second (Chapter 3).py
@@ -0,0 +1,10 @@
+# List
+# Type: List
+# Types: String & Integer
+names = ['ahmed', 'irfan', 'muhammad', 'amir']
+rollno = ['56', '66', '77', '96']
+print (names)
+print (rollno)
+# Index
+print (names[2])
+print (rollno[1])
